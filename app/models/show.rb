59ccb999dac13b9eb64e4794489d7571440b1532
Tristan Gibbs <gtristan89@gmail.com>
2021-07-06 18:08:34 -0500

    commit tree 3e47ad901bab17ca4ebdde297ef9b46e7a1ee7d8

--- app/models/show.rb
+++ app/models/show.rb
@@ -1,3 +1,13 @@
 class Show < ActiveRecord::Base
-  
+    has_many :characters
+    has_many :actor, through: :characters
+    belongs_to :network
+
+    def actors_list
+        self.actor.collect do |a|
+            "#{a.first_name} #{a.last_name}"
+        end
+    end
+
+    
 end
